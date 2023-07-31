db06fd19fd71b27255d6c6802ccf9283f8fba1c4
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-31 01:54:10 +0100

    task 1 Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,9 +39,9 @@ class Server:
         """return the appropriate page of the dataset"""
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
+        idx = index_range(page, page_size)
         data = self.dataset()
 
         if start > len(data):
             return []
-        return data[start: end]
+        return data[idx[0]: idx[1]]
