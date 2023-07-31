409d749d2c54e27ad60c8e21539ecc213d21cbb9
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-31 01:56:04 +0100

    task 1 Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,7 +7,7 @@ default value 1 and page_size with default value 10
 
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
