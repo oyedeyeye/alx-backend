588a706aebebe355718bd270876e0a22efeb0137
Hezekiah Oyemade <oyedey@gmail.com>
2023-08-02 13:37:35 +0100

    task 4. MRU caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+class MRUCache that inherits from BaseCaching and is a caching system:
+"""
+
+
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system
+        Methods:
+            def put(self, key, item):
+            def get(self, key):
+    """
+    def __init__(self):
+        """Initializes the class instance"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """adds to the cache"""
+        if key is not None and item is not None:
+            if key not in self.cache_data.keys():
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    last = self.cache_data.popitem(last=False)
+                    print("DISCARD: {}".format(last[0]))
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key."""
+        if key is not None and key in self.cache_data.keys():
+            self.cache_data.move_to_end(key, last=False)
+            return self.cache_data.get(key)
+        return None
